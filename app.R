39ef98c988073ac42dcf0ea8626b1eeefa7a40a5
rebecacarvalho <40205325+rebecacarvalho@users.noreply.github.com>
2019-04-01 14:58:16 -0300

    Initial commit

---
+++ app.R
@@ -0,0 +1,67 @@
+
+# Titulo: Shiny SJC
+# Autor: Rebeca Carvalho
+
+
+rm(list = ls())
+
+
+# Pacotes utilizados
+
+library(cepespR)
+library(knitr)
+library(plyr)
+library(tidyverse)
+library(lubridate)
+library(shiny)
+library(shinyalert)
+library(shinyBS)
+library(ggplot2)
+library(shiny)
+library(readr)
+library(shiny)
+library(shinythemes)
+library(magrittr)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyjs)
+library(plotly)
+library(DT)
+
+# 1. Data -----------------------------------------------------------------
+
+
+
+# 2. User interface -------------------------------------------------------
+
+ui <- fluidPage(
+  
+ 
+       navbarPage("DadosSJC", theme = shinytheme("flatly"),
+           
+            tabPanel("Caracterização do município"),
+             
+             mainPanel(
+               
+               )))
+
+       
+      
+# 3. Server ---------------------------------------------------------------
+
+server <- function(input, output,session){
+}
+
+
+
+
+
+
+# 4. ShinyApp -------------------------------------------------------------
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
