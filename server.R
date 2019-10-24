42f1e355aadc2e7f532ed6996ebb7b4acd348092
Rebeca Carvalho <rebeca.jesus.carvalho@usp.br>
2019-10-24 13:22:26 -0300

    testes de interface

--- server.R
+++ server.R
@@ -84,17 +84,16 @@ server <- function(input, output, session) {
       ) 
   })
   
-## Definicao do popup de cada regiao 
+
   
-  observe({
-    
-    event <- input$mymap_shape_click
-    
-   
-    
-  }) 
+
+# 3. Popup ----------------------------------------------------------------
+
+## Definicao das caracteristicas de cada regiao
   
-  output$def_mat <- renderUI({
+### Centro
+  
+  output$def_center <- renderUI({
     def <- paste0(
       "<h4 align = 'justify'>
               <p>População: 72.115
@@ -106,7 +105,106 @@ server <- function(input, output, session) {
     HTML(def)
     
   })
- 
+  
+  ### Extremo Norte
+  
+  output$def_exnort <- renderUI({
+    def <- paste0(
+      "<h4 align = 'justify'>
+              <p>População: 15.514
+              <p>Àrea da macrozona (km²): 696,47
+              <p>Densidade demográfica (hab/km²): 22,28
+              <p>Renda média: R$ 574,00</h4>
+              <h4 align ='center'><strong>
+              <p><br/>")
+    HTML(def)
+    
+  })
+  
+  ### Leste
+  
+  output$def_leste <- renderUI({
+    def <- paste0(
+      "<h4 align = 'justify'>
+              <p>População: 160.990
+              <p>Àrea da macrozona (km²): 134,69
+              <p>Densidade demográfica (hab/km²): 1.195,26
+              <p>Renda média: R$ 696,00</h4>
+              <h4 align ='center'><strong>
+              <p><br/>")
+    HTML(def)
+    
+  })
+  
+  ### Norte
+  
+  output$def_nort <- renderUI({
+    def <- paste0(
+      "<h4 align = 'justify'>
+              <p>População: 59.800
+              <p>Àrea da macrozona (km²): 63,73
+              <p>Densidade demográfica (hab/km²): 938,33
+              <p>Renda média: R$ 626,00</h4>
+              <h4 align ='center'><strong>
+              <p><br/>")
+    HTML(def)
+    
+  })
+  
+  ### Oeste
+  
+  output$def_oeste <- renderUI({
+    def <- paste0(
+      "<h4 align = 'justify'>
+              <p>População: 41.163
+              <p>Àrea da macrozona (km²): 44,01
+              <p>Densidade demográfica (hab/km²): 935,31
+              <p>Renda média: R$ 2.519,00</h4>
+              <h4 align ='center'><strong>
+              <p><br/>")
+    HTML(def)
+    
+  })
+  
+  ### Sudeste
+  
+  output$def_sudest <- renderUI({
+    def <- paste0(
+      "<h4 align = 'justify'>
+              <p>População: 46.803
+              <p>Àrea da macrozona (km²): 84,70
+              <p>Densidade demográfica (hab/km²): 552,57
+              <p>Renda média: R$ 653,00</h4>
+              <h4 align ='center'><strong>
+              <p><br/>")
+    HTML(def)
+    
+  })
+  
+  ### Sul
+  
+  output$def_sul <- renderUI({
+    def <- paste0(
+      "<h4 align = 'justify'>
+              <p>População: 233.536
+              <p>Àrea da macrozona (km²): 56,51
+              <p>Densidade demográfica (hab/km²): 4.132,65
+              <p>Renda média: R$ 934,00</h4>
+              <h4 align ='center'><strong>
+              <p><br/>")
+    HTML(def)
+    
+  })
+  
+## Definicao do popup de cada regiao 
+  
+  observe({
+    
+    event <- input$mymap_shape_click
+    
+  }) 
+  
+  
   output$plot <- renderPlot({
   event <- input$mymap_shape_click
    if(is.null(event)){
@@ -119,12 +217,16 @@ server <- function(input, output, session) {
                       tags$h3(align = "center",
                               "Centro")),
          footer = modalButton("Fechar"), 
-         size = "l",
+         size = "m",
          fluidRow(
            column(12,
-         htmlOutput("def_mat")),
+         htmlOutput("def_center")),
          column(12,
-         plotlyOutput("matriculas", height = 310, inline = TRUE))),
+                plotlyOutput("matriculas", height = 310, inline = TRUE),
+                br(),
+                br()),
+         column(12,
+                plotlyOutput("empregos", height = 310, inline = TRUE))),
          easyClose = TRUE,
          style = "
                           overflow: hidden;
@@ -138,20 +240,174 @@ server <- function(input, output, session) {
                           width: auto;
                           "))
      
-    }else if(event =="Norte"){
-      barplot(rnorm(10), col=rgb(0.1,0.4,0.9,0.3))
+    }else if(event =="Extremo Norte"){
+      
+      showModal(modalDialog(
+        title = list(tags$h4(align = "right",
+                             modalButton(label =  NULL,
+                                         icon = icon("times"))),
+                     tags$h3(align = "center",
+                             "Extremo Norte")),
+        footer = modalButton("Fechar"), 
+        size = "m",
+        fluidRow(
+          column(12,
+                 htmlOutput("def_exnort")),
+          column(12,
+                 plotlyOutput("matriculas", height = 310, inline = TRUE))),
+        easyClose = TRUE,
+        style = "
+                          overflow: hidden;
+                          overflow-y: scroll;
+                          flex: 1 1 auto;
+                          padding: 1rem;
+                          max-width: 850px;
+                          margin: 1.75rem auto;
+                          max-height: 500px;
+                          display: flex;
+                          width: auto;
+                          "))
+    } else if(event == "Leste"){
+      showModal(modalDialog(
+        title = list(tags$h4(align = "right",
+                             modalButton(label =  NULL,
+                                         icon = icon("times"))),
+                     tags$h3(align = "center",
+                             "Leste")),
+        footer = modalButton("Fechar"), 
+        size = "m",
+        fluidRow(
+          column(12,
+                 htmlOutput("def_leste")),
+          column(12,
+                 plotlyOutput("matriculas", height = 310, inline = TRUE))),
+        easyClose = TRUE,
+        style = "
+                          overflow: hidden;
+                          overflow-y: scroll;
+                          flex: 1 1 auto;
+                          padding: 1rem;
+                          max-width: 850px;
+                          margin: 1.75rem auto;
+                          max-height: 500px;
+                          display: flex;
+                          width: auto;
+                          "))
+    } else if(event == "Norte"){
+      showModal(modalDialog(
+        title = list(tags$h4(align = "right",
+                             modalButton(label =  NULL,
+                                         icon = icon("times"))),
+                     tags$h3(align = "center",
+                             "Norte")),
+        footer = modalButton("Fechar"), 
+        size = "m",
+        fluidRow(
+          column(12,
+                 htmlOutput("def_nort")),
+          column(12,
+                 plotlyOutput("matriculas", height = 310, inline = TRUE))),
+        easyClose = TRUE,
+        style = "
+                          overflow: hidden;
+                          overflow-y: scroll;
+                          flex: 1 1 auto;
+                          padding: 1rem;
+                          max-width: 850px;
+                          margin: 1.75rem auto;
+                          max-height: 500px;
+                          display: flex;
+                          width: auto;
+                          "))
+    } else if(event == "Oeste"){
+      showModal(modalDialog(
+        title = list(tags$h4(align = "right",
+                             modalButton(label =  NULL,
+                                         icon = icon("times"))),
+                     tags$h3(align = "center",
+                             "Oeste")),
+        footer = modalButton("Fechar"), 
+        size = "m",
+        fluidRow(
+          column(12,
+                 htmlOutput("def_oeste")),
+          column(12,
+                 plotlyOutput("matriculas", height = 310, inline = TRUE))),
+        easyClose = TRUE,
+        style = "
+        overflow: hidden;
+        overflow-y: scroll;
+        flex: 1 1 auto;
+        padding: 1rem;
+        max-width: 850px;
+        margin: 1.75rem auto;
+        max-height: 500px;
+        display: flex;
+        width: auto;
+        "))
+    } else if(event == "Sudeste"){
+      showModal(modalDialog(
+        title = list(tags$h4(align = "right",
+                             modalButton(label =  NULL,
+                                         icon = icon("times"))),
+                     tags$h3(align = "center",
+                             "Sudeste")),
+        footer = modalButton("Fechar"), 
+        size = "m",
+        fluidRow(
+          column(12,
+                 htmlOutput("def_sudest")),
+          column(12,
+                 plotlyOutput("matriculas", height = 310, inline = TRUE))),
+        easyClose = TRUE,
+        style = "
+        overflow: hidden;
+        overflow-y: scroll;
+        flex: 1 1 auto;
+        padding: 1rem;
+        max-width: 850px;
+        margin: 1.75rem auto;
+        max-height: 500px;
+        display: flex;
+        width: auto;
+        "))
+    } else if(event == "Sul"){ 
+  showModal(modalDialog(
+      title = list(tags$h4(align = "right",
+                           modalButton(label =  NULL,
+                                       icon = icon("times"))),
+                   tags$h3(align = "center",
+                           "Sul")),
+      footer = modalButton("Fechar"), 
+      size = "m",
+      fluidRow(
+        column(12,
+               htmlOutput("def_sul")),
+        column(12,
+               plotlyOutput("matriculas", height = 310, inline = TRUE))),
+      easyClose = TRUE,
+      style = "
+                          overflow: hidden;
+                          overflow-y: scroll;
+                          flex: 1 1 auto;
+                          padding: 1rem;
+                          max-width: 850px;
+                          margin: 1.75rem auto;
+                          max-height: 500px;
+                          display: flex;
+                          width: auto;
+                          "))
     }
-  
   
   })
   
   
 
-# 3. Graficos -------------------------------------------------------------
+# 4. Graficos -------------------------------------------------------------
   
   paleta2 <- c("#f39c18","#007479", "#e95b23",  "#93145a","#1d4f24","#66388D", "#C56416", "#008FD6")
 
-# 3.1. Matriculas ---------------------------------------------------------
+# 4.1. Matriculas ---------------------------------------------------------
 
   matriculas$n <- as.numeric(matriculas$n)
   
@@ -176,12 +432,38 @@ server <- function(input, output, session) {
           plot.title = element_text(hjust = 0.5, face = "bold"),
           panel.background = element_blank(),
           plot.margin=margins,
-          legend.justification = "center",
+          legend.justification = "justify",
           legend.position=c(2, 0),
           axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)))
   })
   
 
+# 4.2. Empregos -----------------------------------------------------------
+
+ rais$Trabalhadores <- as.numeric(rais$Trabalhadores)
+  
+  output$empregos <- renderPlotly({
+    event <- input$mymap_shape_click
+    rais2 <- rais %>% 
+      filter(`Região` == event$id)
+    ggplotly( 
+      ggplot(rais2,aes(`Região`, Trabalhadores, fill = Setor)) +
+        geom_bar(stat = "identity", position = "fill") +
+        scale_y_continuous(labels = percent_format()) +
+        coord_flip()+
+        labs(
+          title = "Relação de empregos por por área de atividade",
+          fill = "   Macrozona")+
+        ylab("Porcentagem de trabalhadores") +
+        xlab("")+
+        scale_fill_manual(values = paleta2)+
+        theme(
+          plot.title = element_text(hjust = 0.65, face = "bold"),
+          plot.margin=margins,
+          panel.background = element_blank(),
+          axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)))
+  })
+  
 }
   
 
--- ui.R
+++ ui.R
@@ -6,51 +6,120 @@ ui <-
     title = "Sua São José", ## Titulo da pagina do aplicativo em versao web
     
     
-    navbarPage(title = "Sua São José",id = "suasaojose", 
-               theme = shinytheme("simplex"), 
+   
+    tags$head(
+      tags$style(HTML("
+                      .navbar .navbar-nav {
+                      float: right;
+                      margin-right: 300px;
+                      border-color: #5f9ea0;
+                      border: 5px;}
+
+                      .navbar .navbar-header {
+                      float: left;
+                      border-color: #5f9ea0;
+                      border: 5px;}
+
+                      .container, 
+                      .navbar-inner {
+                      border-color: #5f9ea0;
+                      border: 5px;
+                      } 
+
+.navbar-default{
+border: 5px;
+border-color: #5f9ea0;
+}
+                      
+                      .navbar-static-top {
+                      position: static;
+                      margin-bottom: 0px;
+border-color: #5f9ea0;
+border: 5px;
+                      }
+                      "))),
+    tags$div(class = "navbar-header",
+             style = 
+               "height: 20px;
+             position: relative;
+             padding: 20px 45px;
+             left: 0px;
+             top: 0px;
+             right: 0px;
+             font-size: 200px;
+             width: 100%;
+             align-items: center;
+             padding-left: 20px;
+             border: #1897d5;
+             background-color: 	#D4E6F1;
+             border-width: medium;
+             "  ),
+    
+    navbarPage(title = h1(style="vertical-align:middle;
+                          margin-top:-20px;
+                          margin-left: 340px;
+                          font-family: monospace","Sua São José"),id = "suasaojose", 
+               theme = shinytheme("simplex"),
                
                
-             
-               tabPanel("Caracterização do município",   ## Definicao das ferramentas de selecao para a guia
+               
+               tabPanel(h4("Caracterização do município"),   ## Definicao das ferramentas de selecao para a guia
                         ##  'Caracterizacao do municipio'
+                        
                         
                         sidebarLayout( 
                           
-                          div(id ="step1",
-                              div(id ="Sidebar1",sidebarPanel(h5(align = "center","Faça sua consulta:"),width = 3,
+                        
+                          
+                        sidebarPanel(h5(align = "center","Faça sua consulta:"),width = 12,
+                                     style="margin-left:1.5vw;
+                                     min-height: 20px;
+                                     padding: 40px;
+                                     padding-top:5px;
+                                     padding-left: 180px;
+                                     padding-right: 0px;
+                                     margin-bottom: 25px;
+                                     align-items: center;",
+                                     
                                                               
                                                               
-                                                              
+                                     column(4, align = "center",                               
                                                               selectizeInput(inputId = "AGREGACAO",
                                                                              label = NULL, 
                                                                              choices = c("", "Macrozona",
                                                                                          "Zona de tráfego"),
                                                                              selected = NULL,
-                                                                             options = list(placeholder = 'Escolha uma agregação')),
+                                                                             options = list(placeholder = 'Escolha uma agregação'))),
                                                               
+                                    column(4, align = "center", 
                                                               selectizeInput(inputId = "OBJETIVO",
                                                                              label = NULL, 
                                                                              choices = c("", "Destino",
                                                                                          "Origem"),
                                                                              selected = NULL,
-                                                                             options = list(placeholder = 'Escolha o objetivo da viagem')),
+                                                                             options = list(placeholder = 'Escolha o objetivo da viagem'))),
+                                    column(4, 
                                                               
                                                               
-                                                              h5(align = "center",
+                                                              
                                                                  actionButton(inputId = "BCALC1",
                                                                               label = strong("Atualizar"), ## Botao de acao calcular
                                                                               width = "50%"))
                                                               
                                                               
-                              ))),
+                            ),
                           
                           
-                          mainPanel(id = "Main1",
-                                    
-                          column(12,          
+                          mainPanel(id = "Main1", width = 10,
+                                    style="margin-left:8vw;",      
+                          column(12,
+                                 br(),
+                                 br(),
+                                 br(),
+                                
                           leafletOutput("mymap", 
                                         width = "100%",
-                                        height = 1000)),
+                                        height = 750)),
                           
                           plotOutput("plot", height="300px"),
                           br()
@@ -58,7 +127,9 @@ ui <-
                          
                                     ))),
                
-               tabPanel("Sobre",
+               tabPanel(h4("Transportes")),
+               
+               tabPanel(h4("Sobre"),
                         h4(align = "center",
                         column(8,offset = 2,
                         htmlOutput("sobre")))),
@@ -81,7 +152,7 @@ ui <-
                 text-align: left;
                 z-index: 10;
                 height: 3em;
-                margin-top: 45em;",
+                margin-top: 20em;",
                 
                 tags$div(class = "rodape-container",
                          
